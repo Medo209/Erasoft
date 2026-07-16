d717e23800d7cc15df6328be6e603d8c4c8e2212
Muhamed Sulatn <mohsala120@gmail.com>
2026-07-16 10:53:00 +0300

    added new file task_1.dart

---
+++ task_1.dart
@@ -0,0 +1,32 @@
+//Q 1
+void main()
+{
+  int hour = DateTime.now().hour;
+  print(hour);
+}
+
+//Q 2
+// void main() {
+//   String firstName = "Mohamed";
+//   String lastName = "Salah";
+//   print(firstName);
+//   print(lastName);
+// }
+
+//Q 3
+// void main() {
+//   double kilogram = 74.6;
+//   print(kilogram);
+// }
+
+//Q 4
+// void main() {
+//   bool isStudent = true;
+//   print(isStudent);
+// }
+
+//Q 5
+// void main() {
+//   var city = "shubrakhit";
+//   print(city);
+// }
