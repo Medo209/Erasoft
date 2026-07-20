687ac40ff8bdc0c14f7ed725491ece3c75ce493b
Muhamed Sulatn <mohsala120@gmail.com>
2026-07-20 21:23:36 +0300

    add some instructions

--- task_2.dart
+++ task_2.dart
@@ -1,10 +1,10 @@
-//Q:1
-// void main() {
-//   for (int i = 1; i <= 20; i++)
-//   print(i);
-// }
+Q:1
+void main() {
+  for (int i = 1; i <= 20; i++)
+  print(i);
+}
 
-//Q:2
+Q:2
 void main() {
   List<String> food = ['meat','sweets','fruit','chiken','fish'];
   for(String item in food)
@@ -12,28 +12,28 @@ void main() {
     print(item);
   }
 }
-//Q:3
-//Q:4
-// void main() {
-//   int number = 8;
-//   switch (number) {
-//     case 1:print('monday');
-//     case 2:print('tuesday');
-//     case 3:print('wednesday');
-//     case 4:print('thursday');
-//     case 5:print('friday');
-//     case 6:print('saturday');
-//     case 7:print('sunday');
-//     default:print('invalid number');
-//   }
-// }
-//Q:5
-//Q:6
-// void main() {
-//   int counter = 0;
-//   while (counter < 10) {
-//     print('Hello');
-//     counter++;
-//   }
-// }
-//Q:7
+Q:3
+Q:4
+void main() {
+  int number = 8;
+  switch (number) {
+    case 1:print('monday');
+    case 2:print('tuesday');
+    case 3:print('wednesday');
+    case 4:print('thursday');
+    case 5:print('friday');
+    case 6:print('saturday');
+    case 7:print('sunday');
+    default:print('invalid number');
+  }
+}
+Q:5
+Q:6
+void main() {
+  int counter = 0;
+  while (counter < 10) {
+    print('Hello');
+    counter++;
+  }
+}
+Q:7
