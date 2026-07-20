0c81adce4ccd2d441bb5a1bf2214be87661db908
Muhamed Sulatn <mohsala120@gmail.com>
2026-07-20 21:22:25 +0300

    add some instructions

--- task_2.dart
+++ task_2.dart
@@ -1,36 +1,39 @@
-Q:1
-void main() {
-  for (int i = 1; i <= 20; i++)
-  print(i);
-}
+//Q:1
+// void main() {
+//   for (int i = 1; i <= 20; i++)
+//   print(i);
+// }
 
-Q:2
-void main()
-{
-
-}
-Q:3
-Q:4
+//Q:2
 void main() {
-  int number = 8;
-  switch (number) {
-    case 1:print('monday');
-    case 2:print('tuesday');
-    case 3:print('wednesday');
-    case 4:print('thursday');
-    case 5:print('friday');
-    case 6:print('saturday');
-    case 7:print('sunday');
-    default:print('invalid number');
+  List<String> food = ['meat','sweets','fruit','chiken','fish'];
+  for(String item in food)
+  {
+    print(item);
   }
 }
-Q:5
-Q:6
-void main() {
-  int counter = 0;
-  while (counter < 10) {
-    print('Hello');
-    counter++;
-  }
-}
-Q:7
+//Q:3
+//Q:4
+// void main() {
+//   int number = 8;
+//   switch (number) {
+//     case 1:print('monday');
+//     case 2:print('tuesday');
+//     case 3:print('wednesday');
+//     case 4:print('thursday');
+//     case 5:print('friday');
+//     case 6:print('saturday');
+//     case 7:print('sunday');
+//     default:print('invalid number');
+//   }
+// }
+//Q:5
+//Q:6
+// void main() {
+//   int counter = 0;
+//   while (counter < 10) {
+//     print('Hello');
+//     counter++;
+//   }
+// }
+//Q:7
