863e531d0acfb456ba96dec4ac93456165fbe7cf
Muhamed Sulatn <mohsala120@gmail.com>
2026-07-21 15:18:18 +0300

    add some instructions

--- task_2.dart
+++ task_2.dart
@@ -29,6 +29,28 @@ void main() {
         print('Lastname: ${name[i]}');
     }
   }
+  //Q:4
+  void main() {
+    int number = 8;
+    switch (number) {
+      case 1:
+        print('monday');
+      case 2:
+        print('tuesday');
+      case 3:
+        print('wednesday');
+      case 4:
+        print('thursday');
+      case 5:
+        print('friday');
+      case 6:
+        print('saturday');
+      case 7:
+        print('sunday');
+      default:
+        print('invalid number');
+    }
+  }
 }
 
  // Q:5
@@ -52,26 +74,3 @@ void main() {
     }
   }
 
-
-  //Q:4
-  void main() {
-    int number = 8;
-    switch (number) {
-      case 1:
-        print('monday');
-      case 2:
-        print('tuesday');
-      case 3:
-        print('wednesday');
-      case 4:
-        print('thursday');
-      case 5:
-        print('friday');
-      case 6:
-        print('saturday');
-      case 7:
-        print('sunday');
-      default:
-        print('invalid number');
-    }
-  }
