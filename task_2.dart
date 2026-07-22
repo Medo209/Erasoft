6bdd43c0c420bcbd72c262b24a62f0fd4ad960b8
Muhamed Sulatn <mohsala120@gmail.com>
2026-07-22 22:43:28 +0300

    add some instructions

--- task_2.dart
+++ task_2.dart
@@ -1,76 +1,76 @@
-//Q:1
+// //Q:1
 
-void main() {
-  for (int i = 1; i <= 20; i++) print(i);
+// void main() {
+//   for (int i = 1; i <= 20; i++) print(i);
 
-  // Q:2
+//   // Q:2
 
-  List<String> food = ['meat', 'sweets', 'fruit', 'chiken', 'fish'];
-  for (String item in food) {
-    print(item);
-  }
+//   List<String> food = ['meat', 'sweets', 'fruit', 'chiken', 'fish'];
+//   for (String item in food) {
+//     print(item);
+//   }
 
-  // Q:3
+//   // Q:3
 
-  List<int> number = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-  List<int> even = [];
-  even = number.where((e) {
-    return e.isEven;
-  }).toList();
-  print(even);
-  // Q:7
+//   List<int> number = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+//   List<int> even = [];
+//   even = number.where((e) {
+//     return e.isEven;
+//   }).toList();
+//   print(even);
+//   // Q:7
 
-  List<String> name = ['muhamed', 'salah'];
-  for (int i = 0; i < name.length; i++) {
-    switch (i) {
-      case 0:
-        print('Firstname: ${name[i]}');
-      case 1:
-        print('Lastname: ${name[i]}');
-    }
-  }
-  //Q:4
-  void main() {
-    int number = 8;
-    switch (number) {
-      case 1:
-        print('monday');
-      case 2:
-        print('tuesday');
-      case 3:
-        print('wednesday');
-      case 4:
-        print('thursday');
-      case 5:
-        print('friday');
-      case 6:
-        print('saturday');
-      case 7:
-        print('sunday');
-      default:
-        print('invalid number');
-    }
-  }
-}
+//   List<String> name = ['muhamed', 'salah'];
+//   for (int i = 0; i < name.length; i++) {
+//     switch (i) {
+//       case 0:
+//         print('Firstname: ${name[i]}');
+//       case 1:
+//         print('Lastname: ${name[i]}');
+//     }
+//   }
+//   //Q:4
+//   void main() {
+//     int number = 8;
+//     switch (number) {
+//       case 1:
+//         print('monday');
+//       case 2:
+//         print('tuesday');
+//       case 3:
+//         print('wednesday');
+//       case 4:
+//         print('thursday');
+//       case 5:
+//         print('friday');
+//       case 6:
+//         print('saturday');
+//       case 7:
+//         print('sunday');
+//       default:
+//         print('invalid number');
+//     }
+//   }
+// }
 
- // Q:5
-void main() {
-  List<int> number = [10, 20, 40, 50, 100, 300, 84];
-  int biggest = number[0];
-  for (int i = 1; i < number.length; i++) {
-    if (number[i] > biggest) {
-      biggest = number[i];
-    }
-  }
-  print(biggest);
-}
- // Q:6
+//  // Q:5
+// void main() {
+//   List<int> number = [10, 20, 40, 50, 100, 300, 84];
+//   int biggest = number[0];
+//   for (int i = 1; i < number.length; i++) {
+//     if (number[i] > biggest) {
+//       biggest = number[i];
+//     }
+//   }
+//   print(biggest);
+// }
+//  // Q:6
 
-  void main() {
-    int counter = 0;
-    while (counter < 10) {
-      print('Hello');
-      counter++;
-    }
-  }
+//   void main() {
+//     int counter = 0;
+//     while (counter < 10) {
+//       print('Hello');
+//       counter++;
+//     }
+//   }
 
---
+++ task_3.dart
@@ -0,0 +1,10 @@
+// Q:1
+void main() {
+  for (int i = 1; i <= 30; i++) {
+    if (i % 3 == 0) {
+      print("Fizz");
+    } else {
+      print(i);
+    }
+  }
+}
