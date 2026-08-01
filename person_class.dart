37e641133dcbcd112ee7eb6302d401896dc0d155
Muhamed Sulatn <mohsala120@gmail.com>
2026-08-01 15:47:56 +0300

    class

--- person_class.dart
+++ person_class.dart
@@ -11,3 +11,70 @@ class Person {
     print('$city');
   }
 }
+
+class Rectangle {
+  double? width;
+  double? height;
+
+  Rectangle(this.width, this.height);
+  double area() {
+    return width! * height!;
+  }
+
+  double perimeter() {
+    return 2 * (width! + height!);
+  }
+}
+
+class BankAccount {
+  String? owner;
+  double? balance;
+
+  BankAccount(this.owner, this.balance);
+  BankAccount.empty(this.owner) : balance = 0;
+  void deposit(double amount) {
+    balance = balance! + amount;
+  }
+
+  void withdraw(double amount) {
+    if (amount <= balance!) {
+      balance = balance! - amount;
+    } else {
+      print("Insufficient balance");
+    }
+  }
+}
+
+class Student {
+  String? name;
+  List<double>? grades;
+
+  Student(this.name, this.grades);
+  double average() {
+    double sum = 0;
+    for (double grade in grades!) {
+      sum += grade;
+    }
+    return sum / grades!.length;
+  }
+
+  String status() {
+    if (average() >= 60) {
+      return "Pass";
+    } else {
+      return "Fail";
+    }
+  }
+}
+
+class Product {
+  String? name;
+  double? price;
+  bool? inStock;
+  Product(this.name, this.price, [this.inStock = true]);
+  Product.outOfStock(this.name, this.price) : inStock = false;
+  String toString()
+  {
+    return 'Name: $name, Price: $price, In Stock: $inStock';
+  }
+}
--- task_5.dart
+++ task_5.dart
@@ -1,8 +1,48 @@
-import 'person_class.dart';
-// Q:1
-void main() {
-  Person p1 = Person("Ali", 20, "cairo");
-  Person p2 = Person("Anas",24,"Alex");
-  p1.printInfo();
-  p2.printInfo();
-}
+// import 'person_class.dart';
+
+// // Q:1
+// void main()
+// {
+//   Person p1 = Person("Ali", 20, "cairo");
+//   Person p2 = Person("Anas", 24, "Alex");
+//   p1.printInfo();
+//   p2.printInfo();
+// }
+
+// // Q:2
+// void main()
+// {
+//   Rectangle r = Rectangle(5, 8);
+//   print(r.area());
+//   print(r.perimeter());
+// }
+
+// // Q:3
+// void main()
+// {
+//   BankAccount b1 = BankAccount("Anas", 2000);
+//   b1.deposit(100);
+//   b1.withdraw(500);
+//   print(b1.owner);
+//   print(b1.balance);
+// }
+
+// // Q:4
+// void main()
+// {
+//   Student s = Student("Ali", [40, 80, 70, 60]);
+//   print('${s.name}');
+//   print('${s.average()}');
+//   print('${s.status()}');
+// }
+
+// // Q:5
+// void main()
+// {
+//   Product p1 = Product("Apple", 20);
+//   Product p2 = Product("Orange", 30,);
+//   Product p3 = Product.outOfStock("Banana", 30);
+//   print(p1);
+//   print(p2);
+//   print(p3);
+// }
