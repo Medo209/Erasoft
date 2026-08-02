dfd7fd41f7d5046a084b33a80174a3a76a46fda7
Muhamed Sulatn <mohsala120@gmail.com>
2026-08-02 18:48:23 +0300

    class

--- task_6.dart
+++ task_6.dart
@@ -1,17 +1,17 @@
 import 'classes_2.dart';
 // Q:1
-// void main() {
-// Car c1 = Car(brand: 'BMW', model: 'AMG GT 4-Door', year: 2024);
-// Car c2 = Car(brand:'Toyota', model: 'corola', year: 2022);
-// c1.displayInfo();
-// print('-------------');
-// c2.displayInfo();
-// }
-// Q:2
-// void main(){
-//   Student s1 = Student(name:"Anas", age:22, grade:95);
-//   Student s2 = Student.guest();
-//   s1.showInfo();
-//   s2.showInfo();
-// }
+void main() {
+Car c1 = Car(brand: 'BMW', model: 'AMG GT 4-Door', year: 2024);
+Car c2 = Car(brand:'Toyota', model: 'corola', year: 2022);
+c1.displayInfo();
+print('-------------');
+c2.displayInfo();
+}
+Q:2
+void main(){
+  Student s1 = Student(name:"Anas", age:22, grade:95);
+  Student s2 = Student.guest();
+  s1.showInfo();
+  s2.showInfo();
+}
 // Q:3
